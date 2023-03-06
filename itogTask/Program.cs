0a1244a9a38b1beadff2fe49ad239b3278ebdee5
Elizabeth <elizaveta.bezumnova@gmail.com>
2023-03-06 18:08:43 +0300

    поиск строк с количеством символом меньше 3

---
+++ itogTask/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+ длина которых меньше или равна 3 символа */
+
+string[] beforeArray = new string[] { "hello", "2", "world", ":-)"};
+string[] afterArray = new string[beforeArray.Length];
+int j = 0;
+
+for (int i = 0; i < beforeArray.Length; i++)
+{
+    if (beforeArray[i].Length >=3)
+    {
+        afterArray[j] = beforeArray[i];
+        j++;
+    }
+}
